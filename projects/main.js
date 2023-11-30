7b69e255f286254fe128bb9d2dcf43f45fd162a1
brpe1954 <brett.perez@colorado.edu>
2023-11-30 14:30:11 -0700

    balls!

---
+++ projects/main.js
@@ -0,0 +1,118 @@
+// setup canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+// Ball class definition
+
+class Ball {
+
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  // function to command our browser to draw a ball on the webpage
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+  
+  // function to add movement to the balls
+  update() {
+
+    // first 4 if statements check to see if a ball has reached the edge of the webpage
+    if ((this.x + this.size) >= width) {
+      this.velX = -(this.velX); // if it has reached an edge, reverse the direction of the movement
+    }
+  
+    if ((this.x - this.size) <= 0) {
+      this.velX = -(this.velX);
+    }
+  
+    if ((this.y + this.size) >= height) {
+      this.velY = -(this.velY);
+    }
+  
+    if ((this.y - this.size) <= 0) {
+      this.velY = -(this.velY);
+    }
+    
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  // randomly update the color of a ball when a collision is detected
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }  
+  
+}
+
+// create an array of 25 balls to add to the webpage
+const balls = [];
+
+// while loop defines how many balls to add
+while (balls.length < 10) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size,
+  );
+
+  balls.push(ball);
+}
+
+// animation loop
+function loop() {
+  ctx.fillStyle = "rgb(0 0 0 / 25%)"; // a semi-transparent fill allows shows the previous few animation frames to show--producing a trail effect
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
